b6362f44e2c889f4b9bcb1b86c1160600d44647e
Lars Lorch <Lars@Larss-MacBook-Pro.local>
2016-11-29 19:36:31 -0500

    Initial Commit

---
+++ The Billboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Billboard
+//
+//  Created by Lars Lorch on 11/29/16.
+//  Copyright © 2016 The Billboard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
