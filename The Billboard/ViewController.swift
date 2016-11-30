d669d7c41b4ab1967135214dcc2769c1b1a999ac
Lars Lorch <Lars@larss-macbook-pro.local>
2016-11-29 21:04:00 -0500

    11/29 First functionality with register

--- The Billboard/ViewController.swift
+++ The Billboard/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     
  
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +27,14 @@ class ViewController: UIViewController {
     @IBAction func loginButton(_ sender: UIButton) {
     }
     
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
+    
+    
+    // Segues
+    
+    @IBAction func unwindToThisViewController(segue: UIStoryboardSegue) {
+    }
     
     
     
